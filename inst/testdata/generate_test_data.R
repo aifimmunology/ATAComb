ca5eca85db23405e660779b1b63df8a2b8898a5e
Lucas Graybuck <lucasg@alleninstitute.org>
2019-12-17 09:33:53 -0800

    adding test data; some io updates to handle conversions to windows.

---
+++ inst/testdata/generate_test_data.R
@@ -0,0 +1,36 @@
+library(ATAComb)
+library(data.table)
+
+outs_dir <- "C:/datasets/atac_perm_cells/full_tenx_run/perm_cell_dig_0.01_analysis"
+
+fragments_file <- file.path(outs_dir, "fragments.tsv.gz")
+singlecell_file <- file.path(outs_dir, "singlecell.csv")
+
+singlecell <- data.table::fread(singlecell_file)
+singlecell <- singlecell[-1,]
+
+min_reads <- 1000
+
+set.seed(3030)
+singlecell_pass_cell <- singlecell[sample(which(passed_filters >= min_reads & is__cell_barcode == 1), 50),]
+singlecell_pass_noncell <- singlecell[sample(which(passed_filters >= min_reads & is__cell_barcode != 1), 50),]
+singlecell_fail <- singlecell[sample(which(passed_filters < min_reads), 100),]
+
+singlecell <- rbind(singlecell_pass_cell,
+                    singlecell_pass_noncell,
+                    singlecell_fail)
+
+fwrite(singlecell,
+       "inst/testdata/outs/singlecell.csv")
+
+fragments <- vroom::vroom(fragments_file,
+                          delim = "\t",
+                          col_names = c("chr","start","end","barcode","n_reads"),
+                          col_types = c(chr = "c", start = "i", end = "i", barcode = "c", n_reads = "i"))
+
+fragments <- fragments[fragments$barcode %in% singlecell$barcode,]
+
+fwrite(fragments,
+       "inst/testdata/outs/fragments.tsv.gz",
+       col.names = FALSE,
+       sep = "\t")
